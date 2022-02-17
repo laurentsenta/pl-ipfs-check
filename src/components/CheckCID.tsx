0f4bfe94ab14eca585dc080ef30ac44c03604d19
Laurent Senta <laurent@singulargarden.com>
2022-02-17 15:59:15 +0100

    content: add footer

--- src/components/CheckCID.tsx
+++ src/components/CheckCID.tsx
@@ -1,116 +1,17 @@
-import {
-  DEFAULT_BACKEND_URL,
-  fetchCIDLogs,
-  IParamsAddrAndCID,
-  IResult,
-  isValidParams,
-} from "data";
+import { fetchCIDLogs, IResult, isValidParams } from "data";
+import { useCommonParams } from "data/useCommonParams";
 import isEmpty from "lodash-es/isEmpty";
-import omitBy from "lodash-es/omitBy";
-import {
-  ChangeEventHandler,
-  FormEventHandler,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from "react";
+import { FormEventHandler, useCallback } from "react";
 import { useMutation } from "react-query";
-import { useSearchParams } from "react-router-dom";
 import { Message } from "./Message";
 
-const Result: React.FC<{ data: IResult }> = ({ data }) => {
-  return (
-    <>
-      {data.ConnectionError !== "" ? (
-        <Message
-          failure
-          title="Could not connect to multiaddr"
-          content={data.ConnectionError}
-        />
-      ) : (
-        <Message success title="Successfully connected to multiaddr" />
-      )}
-      {isEmpty(data.PeerFoundInDHT) ? (
-        <Message
-          failure
-          title="Could not find any multiaddrs in the dht"
-          content={data.ConnectionError}
-        />
-      ) : (
-        <Message success title="Found multiaddrs advertised in the DHT">
-          <ul>
-            {Object.entries(data.PeerFoundInDHT).map(([peerAddr, value]) => (
-              <li key={peerAddr}>{peerAddr}</li>
-            ))}
-          </ul>
-        </Message>
-        // TODO: port the idea of finding your address in the multiaddress (l.137 from original web)
-      )}
-      {!data.CidInDHT ? (
-        <Message failure title="Could not find the multihash in the DHT" />
-      ) : (
-        <Message success title="Found multihash advertised in the DHT" />
-      )}
-      {data.DataAvailableOverBitswap.Error ? (
-        <Message
-          failure
-          title="There was an error downloading the CID from the peer"
-          content={data.DataAvailableOverBitswap.Error}
-        />
-      ) : (
-        <Message success title="The data is available over bitswap" />
-        // TODO: port back the other checks
-      )}
-      <pre>{JSON.stringify(data, undefined, 2)}</pre>
-    </>
-  );
-};
-
 export const CheckCID: React.FC = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const s = (key: string, initial: string = ""): string => {
-    const value = searchParams.get(key);
-    return value ? value : initial;
-  };
-
-  const [addr, setAddr] = useState(s("addr"));
-  const [cid, setCID] = useState(s("cid"));
-  const [backend, setBackend] = useState(s("backend", DEFAULT_BACKEND_URL));
-
-  const params = useMemo(
-    () => omitBy({ addr, backend, cid }, isEmpty) as Partial<IParamsAddrAndCID>,
-    [addr, backend, cid]
-  );
+  const { params, onChangeCID, onChangeBackend, onChangeAddr } =
+    useCommonParams();
+  const { cid, backend, addr } = params;
 
   const mutation = useMutation(fetchCIDLogs);
   const canSubmit = isValidParams(params) && !mutation.isLoading;
-
-  const onChangeAddr: ChangeEventHandler<HTMLInputElement> = useCallback(
-    (e) => {
-      setAddr(e.target.value);
-    },
-    [setAddr]
-  );
-
-  const onChangeCID: ChangeEventHandler<HTMLInputElement> = useCallback(
-    (e) => {
-      setCID(e.target.value);
-    },
-    [setCID]
-  );
-
-  const onChangeBackend: ChangeEventHandler<HTMLInputElement> = useCallback(
-    (e) => {
-      setBackend(e.target.value);
-    },
-    [setBackend]
-  );
-
-  useEffect(() => {
-    setSearchParams(params);
-  }, [setSearchParams, params]);
 
   const onSubmit: FormEventHandler<HTMLFormElement> = useCallback(
     (e) => {
@@ -189,5 +90,53 @@ export const CheckCID: React.FC = () => {
         {mutation.data && <Result data={mutation.data} />}
       </div>
     </div>
+  );
+};
+
+const Result: React.FC<{ data: IResult }> = ({ data }) => {
+  return (
+    <>
+      {data.ConnectionError !== "" ? (
+        <Message
+          failure
+          title="Could not connect to multiaddr"
+          content={data.ConnectionError}
+        />
+      ) : (
+        <Message success title="Successfully connected to multiaddr" />
+      )}
+      {isEmpty(data.PeerFoundInDHT) ? (
+        <Message
+          failure
+          title="Could not find any multiaddrs in the dht"
+          content={data.ConnectionError}
+        />
+      ) : (
+        <Message success title="Found multiaddrs advertised in the DHT">
+          <ul>
+            {Object.entries(data.PeerFoundInDHT).map(([peerAddr, value]) => (
+              <li key={peerAddr}>{peerAddr}</li>
+            ))}
+          </ul>
+        </Message>
+        // TODO: port the idea of finding your address in the multiaddress (l.137 from original web)
+      )}
+      {!data.CidInDHT ? (
+        <Message failure title="Could not find the multihash in the DHT" />
+      ) : (
+        <Message success title="Found multihash advertised in the DHT" />
+      )}
+      {data.DataAvailableOverBitswap.Error ? (
+        <Message
+          failure
+          title="There was an error downloading the CID from the peer"
+          content={data.DataAvailableOverBitswap.Error}
+        />
+      ) : (
+        <Message success title="The data is available over bitswap" />
+        // TODO: port back the other checks
+      )}
+      <pre>{JSON.stringify(data, undefined, 2)}</pre>
+    </>
   );
 };
--- src/pages/PageContainer.tsx
+++ src/pages/PageContainer.tsx
@@ -2,12 +2,23 @@ import { Outlet } from "react-router-dom";
 
 export const PageContainer: React.FC = ({ children }) => {
   return (
-    <div className="main py-2">
-      <div className="columns is-centered">
-        <div className="column is-10">
-          <Outlet />
+    <>
+      <div className="main py-2">
+        <div className="columns is-centered">
+          <div className="column is-10">
+            <Outlet />
+          </div>
         </div>
       </div>
-    </div>
+      <footer className="footer mt-4">
+        <div className="content has-text-centered">
+          <p>
+            <strong>PL Diagnose</strong>
+            <br />
+            <a href="https://github.com/laurentsenta/pl-ipfs-check">Github</a>.
+          </p>
+        </div>
+      </footer>
+    </>
   );
 };
